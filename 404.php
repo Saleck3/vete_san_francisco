39ad376900f87c54a2870b2a4ada34bb9f7c6120
Francisco Souza Aguirre <fransaguirre04@gmail.com>
2023-07-14 21:38:52 -0300

    Agregada pagina 404

--- 404.php
+++ 404.php
@@ -1,2 +1,36 @@
-<?php
-echo "404 not found";
+<html>
+
+<head>
+  <link rel="stylesheet" href="public/css/style.css">
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
+</head>
+
+
+<body class=" color-violeta w3-center w3-border-deep-purple	">
+
+  <header class="w3-container w3-center w3-deep-purple">
+    <h1><a class="w3-button" href="index.php">Veterinaria San Francisco</a></h1>
+    <div class="w3-bar">
+      <a class="w3-button w3-purple" href="">Buscar Paciente</a>
+      <a class="w3-button w3-purple" href="">Buscar Dueño</a>
+    </div>
+  </header>
+  <div class="w3-container">
+    <div class="w3-panel w3-deep-purple w3-margin-top" style="width: 50%; margin: auto;">
+      <h2>
+        404 Paciente no encontrado
+      </h2>
+
+      <p>
+        No pudimos encontrar la pagina del paciente que buscabas!<br>
+        Revisa si los datos fueron ingresados correctamente.
+      </p>
+      <a href="index.php" class="w3-btn w3-purple">
+        Volver a inicio
+      </a>
+    </div>
+  </div>
+</body>
+
+</html>
