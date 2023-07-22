5294bcbf418b823f2fc656fbf9b9494bde289a4e
Saleck3 <aledagonale@gmail.com>
2023-07-22 00:21:43 -0300

    Agregados metodos para manejo de base de datos

--- common/CommonModel.php
+++ common/CommonModel.php
@@ -12,12 +12,44 @@ class CommonModel
         try {
             $this->db = new PDO('mysql:host=' . $_ENV["DB_HOST"] . ';dbname=' . $_ENV["DB_NOMBRE"] . ';port=' . $_ENV["DB_PUERTO"] . ';charset=' . $_ENV["DB_CHARSET"], $_ENV["DB_USUARIO"], $_ENV["DB_CLAVE"]);
         } catch (PDOException $e) {
-            $this->logger->logError($e->getMessage());
+            if ($_ENV["DEBUG"]) {
+                $this->logger->logError($e->getMessage());
+            }
             die;
         }
 
         $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $this->db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    }
+
+    /**
+     * Inicia una transaccion en la base de datos
+     *
+     * @return void
+     */
+    public function transaction_begin(): void
+    {
+        $this->db->beginTransaction();
+    }
+
+    /**
+     * Commitea una transaccion
+     *
+     * @return void
+     */
+    public function transaction_commit(): void
+    {
+        $this->db->commit();
+    }
+
+    /**
+     * Hace rollback de una transaccion
+     *
+     * @return void
+     */
+    public function transaction_rollback(): void
+    {
+        $this->db->rollback();
     }
 
     /**
@@ -46,25 +78,7 @@ class CommonModel
         }
 
         $query = "SELECT " . $camposSelect . " FROM " . $tabla . $where;
-        var_dump($query);
-        $result = null;
-        try {
-            $stmt = $this->db->prepare($query);
-
-            if (!empty($toBind) && !empty($where)) {
-                foreach ($toBind as $key => $value) {
-                    $stmt->bindParam($key, $value);
-                }
-            }
-
-            $stmt->execute();
-            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        } catch (Exception $e) {
-            if ($_ENV["DEBUG"]) {
-                $this->logger->logError($e->getMessage());
-            }
-        }
-
+        $result = $this->consulta($query, $toBind);
         if (!empty($columnaID)) {
             $resultAcomodado = array();
             foreach ($result as $linea) {
@@ -78,9 +92,86 @@ class CommonModel
         return $result;
     }
 
-    public function actualizar()
+    /**
+     * Modifica un registro en la base
+     *
+     * @param string $tabla
+     * @param array $datos
+     * @param int $id
+     * @return bool|int|string|null Retorna la cantidad de filas que modifico,si hay error retorna false y si no modificó retorna true
+     */
+    function modificar(string $tabla, array $datos, int $id): bool|int|string|null
     {
+        // Control de campos
+        if (!is_numeric($id) || empty($datos)) {
+            return null; // Si no llega el id retorno NULL
+        }
+        $id_tbl = null;
 
+        if (isset($datos['id_tbl']) && !empty($datos['id_tbl'])) {
+            $id_tbl = $datos['id_tbl'];
+            unset($datos['id_tbl']);
+        }
+
+        $sentencias = array();
+        // Obtengo las claves y valores del array y los separo
+        foreach ($datos as $clave => $valor) {
+            if ($clave != "") {
+                $sentencias[] = $this->sentencia_campos_query($valor, $clave);
+            }
+        }
+        $sentencias = join(",", $sentencias);
+        // Creo la consulta SQL
+        $query = "UPDATE `" . $tabla . "`";
+        $query .= "SET $sentencias ";
+
+        if (!is_null($id_tbl)) {
+            $query .= " WHERE id = $id_tbl";
+        } else {
+            $query .= "WHERE id = '$id'";
+        }
+
+        try {
+            $stmt = $this->db->prepare($query);
+            if (!empty($toBind)) {
+                $this->binds($stmt, $toBind);
+            }
+            $stmt->execute();
+        } catch (Exception $e) {
+            if ($_ENV["DEBUG"]) {
+                $this->logger->logError($e->getMessage());
+            }
+            return false;
+        }
+
+        if ($stmt->rowCount()) {
+            return $stmt->rowCount();
+        } else {
+            return true;
+        }
+    }
+
+    /**
+     * Ejecuta una consulta base
+     *
+     * @param string $query
+     * @param array|null $toBind
+     * @return array|false|void
+     */
+    public function consulta(string $query, array $toBind = null)
+    {
+        try {
+            $stmt = $this->db->prepare($query);
+            if (!empty($toBind)) {
+                $this->binds($stmt, $toBind);
+            }
+            $stmt->execute();
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        } catch (Exception $e) {
+            if ($_ENV["DEBUG"]) {
+                $this->logger->logError($e->getMessage());
+            }
+        }
     }
 
 
@@ -249,12 +340,11 @@ class CommonModel
      *
      * @param PDOStatement $stmt
      * @param array $datos
-     * @return int
+     * @return void
      */
-    private function binds(PDOStatement $stmt, array $datos): int
+    private function binds(PDOStatement $stmt, array $datos): void
     {
         // Obtengo las claves y valores del array y los separo
-        $binds = 0;
         if ($datos) foreach ($datos as $clave => $valor) {
             if (is_numeric($valor)) {
                 $type = PDO::PARAM_INT;
@@ -262,9 +352,7 @@ class CommonModel
                 $type = PDO::PARAM_STR;
             }
             $stmt->bindValue($clave, $valor, $type);
-            $binds++;
         }
-        return $binds;
     }
 
     /**
@@ -285,9 +373,95 @@ class CommonModel
     }
 
 
-    public
-    function borrar()
+    /**
+     * Elimina los elementos de una tabla
+     *
+     * @param string $tabla
+     * @param array|int|null $id
+     * @param string|null $where
+     * @return bool|null TRUE si pudo eliminar FALSE si hubo un error y NULL si no se paso ningun where
+     * @noinspection SqlWithoutWhere
+     */
+    public function borrar(string $tabla, array|int $id = null, string $where = null): ?bool
     {
+        //TODO Reemplazar por eliminado logico
+        if (empty($id) && empty($where)) {
+            return null;
+        }
 
+        if (!empty($where)) {
+            if (!str_contains($where, "where")) {
+                $where = " where " . $where;
+            }
+        } else {
+            if (is_array($id)) {
+                $where = " id in (" . join(" ,", $id) . ")";
+            } else {
+                $where = " id = $id";
+            }
+        }
+
+        $query = "DELETE FROM " . $tabla . $where;
+        try {
+            $stmt = $this->db->prepare($query);
+            if (!empty($toBind)) {
+                $this->binds($stmt, $toBind);
+            }
+            return $stmt->execute();
+        } catch (Exception $e) {
+            if ($_ENV["DEBUG"]) {
+                $this->logger->logError($e->getMessage());
+            }
+            return false;
+        }
+
+    }
+
+
+    /**
+     * Este metodo devolvera el where correspondiente, si se pasa el campo desde y el campo hasta
+     * devolvera el where con el between entre ambos, si se pasa solo el campo hasta devolvera para que filtre por los menores
+     * de ese valor, si se pasa solo desde se devolvera para que filtre por los valores mayores a ese valor.
+     *
+     * @param mixed $campo_base_de_datos campo con el cual se comparara los valores desde hasta
+     * @param mixed|null $desde valor desde el cual quieres comparar por default null
+     * @param mixed|null $hasta valor hasta el cual quieres comparar por default null
+     * @return string
+     */
+    public function where_desde_hasta(mixed $campo_base_de_datos, mixed $desde = null, mixed $hasta = null): string
+    {
+        $this->escapar_mysql($desde);
+        $this->escapar_mysql($hasta);
+        $where = "";
+        if ($desde && $hasta && $desde != $hasta) {
+            $where .= "and $campo_base_de_datos BETWEEN '" . $desde . "' and '" . $hasta . "' ";
+
+
+        } elseif ($desde && $hasta && $desde == $hasta) {
+            $where .= "and $campo_base_de_datos  = '" . $desde . "' ";
+        } elseif ($desde && !$hasta) {
+            $where .= "and $campo_base_de_datos  >= '" . $desde . "' ";
+        } elseif (!$desde && $hasta) {
+            $where .= "and $campo_base_de_datos  <= '" . $hasta . "' ";
+        }
+        return $where;
+    }
+
+    /**
+     * Sanitiza una variable para que sea pasable a MySQL
+     *
+     * @param array|string $string
+     * @return void
+     */
+    public function escapar_mysql(array|string &$string): void
+    {
+        if (is_array($string)) {
+            foreach ($string as $k => $v) {
+                $this->escapar_mysql($v);
+                $string[$k] = $v;
+            }
+        } else {
+            $string = str_replace("\'", "''", filter_var($string, FILTER_SANITIZE_ADD_SLASHES));
+        }
     }
 }
